d9eba2e1befeabcb01ca6e3bf923346fcbe40b94
sHyaMsha <shyamkumar97@live.com>
2020-10-25 19:58:40 +0530

    deploying

--- index.js
+++ index.js
@@ -1,19 +1,17 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3001;
 const path = require("path");
+const mongoose = require("./config/dbConnect");
+
 app.use(express.static(path.join(__dirname, "client/build")));
 app.use(express.json());
 app.use(cors());
-const mongoose = require("./config/dbConnect");
 const { contactRouter } = require("./app/controllers/contactController");
 
-app.get("/", (req, res) => {
-	res.send("welcome to contact manager");
-});
-
 app.use("/", contactRouter);
+
 app.get("*", (req, res) => {
 	res.sendFile(path.join(__dirname + "/client/build/index.html"));
 });
